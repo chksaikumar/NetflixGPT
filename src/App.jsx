4bda77c120cb6cbca0bcde81796a114125790b3a
Saikumar <chksaikumar@gmail.com>
2025-04-15 14:06:08 -0500

    initiated the React with vite webPack

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+const App = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <h1> Hello NetflixGpt is Under constrection</h1>
+    </div>
+  );
+};
+
+export default App;

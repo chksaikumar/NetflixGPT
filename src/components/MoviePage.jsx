29e4f6385411c7988ec283b9e45004a2519c81d0
Saikumar <chksaikumar@gmail.com>
2025-05-02 11:02:42 -0500

    added movie page

--- src/components/MoviePage.jsx
+++ src/components/MoviePage.jsx
@@ -1,56 +1,52 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
-
 import { API_OPTIONS } from "../utils/TMDB";
 import { setMainVideo } from "./Redux/Store/moviesSlice";
 
-// Replace with your actual TMDB API key
-
 const MoviePage = () => {
-  const { id } = useParams(); // Get movie ID from URL
-  const dispach = useDispatch();
-  const [moviekey, setmoviekey] = useState("");
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const [movieKey, setMovieKey] = useState("");
 
   const getMovieVideo = async () => {
-    const MainVideo = await fetch(
-      `
-  https://api.themoviedb.org/3/movie/${id}/videos`,
-      API_OPTIONS
-    );
-    const trailerVideo = await MainVideo.json();
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${id}/videos`,
+        API_OPTIONS
+      );
+      const data = await response.json();
 
-    const video = trailerVideo.results.filter(
-      (video) => video.type === "Trailer"
-    );
-    const finalTrailer = trailerVideo.length
-      ? video[0]
-      : trailerVideo.results[0];
-    console.log(finalTrailer.key);
-    setmoviekey(finalTrailer.key);
-    dispach(setMainVideo(finalTrailer));
+      const trailers = data.results.filter((video) => video.type === "Trailer");
+      const finalTrailer = trailers.length ? trailers[0] : data.results[0];
+
+      if (finalTrailer?.key) {
+        setMovieKey(finalTrailer.key);
+        dispatch(setMainVideo(finalTrailer));
+      }
+    } catch (error) {
+      console.error("Error fetching movie video:", error);
+    }
   };
 
   useEffect(() => {
     getMovieVideo();
-  }, []);
+  }, [id]);
 
   return (
-    <>
-      <div className="relative w-full h-screen">
-        {/* YouTube Iframe - Full Controls */}
+    <div className="relative w-full h-screen bg-black flex items-center justify-center">
+      {movieKey ? (
         <iframe
-          className="w-full h-full object-cover"
-          src={`https://www.youtube.com/embed/${moviekey}?autoplay=1&controls=1&showinfo=1&modestbranding=1&fs=1`}
+          className="w-full h-full"
+          src={`https://www.youtube.com/embed/${movieKey}?autoplay=1&controls=1&showinfo=0&modestbranding=1&fs=1&rel=0`}
           title="Netflix Trailer"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowFullScreen
         ></iframe>
-
-        {/* Optional: Dark Gradient Overlay (remove if you want full viewability) */}
-        <div className="absolute inset-0 bg-gradient-to-r from-black via-transparent to-transparent pointer-events-none"></div>
-      </div>
-    </>
+      ) : (
+        <div className="text-white text-xl">Loading trailer...</div>
+      )}
+    </div>
   );
 };
 
